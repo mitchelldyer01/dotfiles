b34f8cc87265edcda2eadff00555ed36f27dc085
mitchelldyer01@gmail.com <mitchelldyer01@gmail.com>
2023-03-04 20:33:12 -0700

    add diagnostic popup to nvim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -18,3 +18,11 @@ lsp.ensure_installed({
 })
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
--- nvim/lua/mitch/set.lua
+++ nvim/lua/mitch/set.lua
@@ -26,3 +26,4 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
