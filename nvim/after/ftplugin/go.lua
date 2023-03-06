54cb98115184b8a3f962fc9cf210d596fc2bce8f
Mitchell Dyer <mitchelldyer01@gmail.com>
2023-03-06 13:26:53 -0700

    do goimport on saving a .go file

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -2,3 +2,10 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+	pattern = '*.go',
+	callback = function() 
+		vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports'} }, apply = true })
+	end
+})
